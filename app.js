fbe643368348d987c3557402bb36655cfb2fb679
Filip Pesic <filip.pesic.dev@gmail.com>
2020-05-25 02:28:26 +0200

    Fixed validation checks, added a new page, added some logic for player vs. cpu game

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //import {Login} from './login.js';
 
+let pojmovi = db.collection('pojmovi');
 let formUsername = document.querySelector('#formUsername');
 let inputUsername = document.querySelector('#inputUsername');
 let divUser = document.querySelector('#divUser');
 let formPojam = document.querySelector('#formPojam');
 let inputPojam = document.querySelector('#inputPojam');
 let linkPojmovi = document.getElementById('linkPojmovi');
-let pojmovi = db.collection('pojmovi');
+let formStartGame = document.querySelector('#formStartGame');
+let formInputs = document.querySelectorAll('#formStartGame input');
+let startTimer = document.querySelector('#startTimer');
+
 
 //console.log(linkPojmovi);
 
@@ -22,7 +26,7 @@ if (localStorage.username) {
 }
 
 // Postavljanje korisnickog imena u localstorage
-if (location.pathname.slice(1) != 'dodaj_pojam.html') {
+if (location.pathname.includes('index')) {
     formUsername.addEventListener('submit', e => {
         e.preventDefault();
         if (inputUsername.value.replace(/\s+/g, '') == '') {
@@ -35,61 +39,64 @@ if (location.pathname.slice(1) != 'dodaj_pojam.html') {
 }
 
 // Formatiranje podataka za bazu
-if (location.pathname.slice(1) == 'dodaj_pojam.html') {
+if (location.pathname.includes('dodaj_pojam.html')) {
     formPojam.addEventListener('submit', e => {
         e.preventDefault();
         let selektovanPojam = document.querySelector('select').value;
-        if (inputPojam.value.replace(/\s+/g, '') == '' || selektovanPojam == 'Izbaberite kategoriju') {
+
+        if (inputPojam.value.replace(/\s+/g, '') == '' || selektovanPojam == 'Izbaberite kategoriju' || selektovanPojam.replace(/\s+/g, '') == '') {
             alert('Pojam ili kategorija ne moze biti prazna');
-        }
-        let noWhitespace = inputPojam.value.replace(/\s+/g, '').toLowerCase();
-        let formatiranPojam = noWhitespace.charAt(0).toUpperCase() + noWhitespace.slice(1);
-        //console.log(document.querySelector('select').value);
-        //console.log(formatiranPojam);
-        let specialPocetnoSlovo;
-        console.log('-------------------------');
-
-        if (noWhitespace.slice(0, 2) == 'nj' || noWhitespace.slice(0, 2) == 'lj' || noWhitespace.slice(0, 2) == 'dž') {
-            specialPocetnoSlovo = noWhitespace.charAt(0).toUpperCase() + noWhitespace.slice(1, 2);
         } else {
-            specialPocetnoSlovo = noWhitespace.charAt(0).toUpperCase();
+            let noWhitespace = inputPojam.value.replace(/\s+/g, '').toLowerCase();
+            let formatiranPojam = noWhitespace.charAt(0).toUpperCase() + noWhitespace.slice(1);
+            let specialPocetnoSlovo;
+            //console.log(document.querySelector('select').value);
+            //console.log(formatiranPojam);
+
+            if (noWhitespace.slice(0, 2) == 'nj' || noWhitespace.slice(0, 2) == 'lj' || noWhitespace.slice(0, 2) == 'dž') {
+                specialPocetnoSlovo = noWhitespace.charAt(0).toUpperCase() + noWhitespace.charAt(1);
+            } else {
+                specialPocetnoSlovo = noWhitespace.charAt(0).toUpperCase();
+            }
+
+            // Provera i dodavanje da li izabrani pojam vec postoji u bazi
+            pojmovi.where('kategorija', '==', selektovanPojam)
+                .where('pojam', '==', formatiranPojam)
+                .get()
+                .then(snapshot => {
+                    if (snapshot.docs.length) {
+                        alert(`Pojam '${formatiranPojam}' vec postoji za izabranu kategoriju: ${selektovanPojam}`);
+                        //console.log(snapshot.docs.length);
+                    } else {
+                        let currentDate = new Date();
+                        if (localStorage.username) {
+                            pojmovi.doc().set({
+                                kategorija: selektovanPojam,
+                                korisnik: localStorage.username,
+                                pocetnoSlovo: specialPocetnoSlovo,
+                                pojam: formatiranPojam,
+                                vreme: firebase.firestore.Timestamp.fromDate(currentDate)
+                            })
+                                .then(() => {
+                                    alert(`Uspesno ste dodali pojam '${formatiranPojam}' sa kategorijom: ${selektovanPojam} u bazu`);
+                                })
+                                .catch(error => {
+                                    console.error("Cannot get documents from collection: ", error);
+                                });
+                            formPojam.reset();
+                        } else {
+                            alert('Upis ne moze da se izvrsi, korisnicko ime nije postaljveno!')
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.error("Cannot get documents from collection: ", error);
+                });
         }
-
-        // Provera i dodavanje da li izabrani pojam vec postoji u bazi
-        pojmovi.where('kategorija', '==', selektovanPojam)
-            .where('pojam', '==', formatiranPojam)
-            .get()
-            .then(snapshot => {
-                if (snapshot.docs.length) {
-                    alert(`Pojam '${formatiranPojam}' vec postoji za izabranu kategoriju: ${selektovanPojam}`);
-                    console.log(snapshot.docs.length);
-                } else {
-                    let currentDate = new Date();
-
-                    pojmovi.doc().set({
-                        kategorija: selektovanPojam,
-                        korisnik: localStorage.username,
-                        pocetnoSlovo: specialPocetnoSlovo,
-                        pojam: formatiranPojam,
-                        vreme: firebase.firestore.Timestamp.fromDate(currentDate)
-                    })
-                        .then(() => {
-                            alert(`Uspesno ste dodali pojam '${formatiranPojam}' sa kategorijom: ${selektovanPojam} u bazu`);
-                        })
-                        .catch(error => {
-                            console.error("Cannot get documents from collection: ", error);
-                        });
-                    formPojam.reset();
-                }
-            })
-            .catch(error => {
-                console.error("Cannot get documents from collection: ", error);
-            });
-
     });
 }
 
-
+// Hall Of Fame no front-end yet
 pojmovi
     .orderBy('korisnik', 'asc')
     .get()
@@ -132,14 +139,74 @@ pojmovi
         }
     );
 
+if (location.pathname.includes('covek_vs_cpu.html')) {
 
-// db.collection('pojmovi').get()
-//     .then(function (querySnapshot) {
-//         querySnapshot.forEach(function (doc) {
-//             console.log(doc.id, " => ", doc.data());
-//         });
+    let abeceda = ['A', 'B', 'C', 'Č', 'Ć', 'D', 'Dž', 'Đ', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Lj', 'N', 'M', 'Nj', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+    //let radnomSlovo = abeceda[Math.floor(Math.random() * abeceda.length)];
+    //document.querySelector('#rSlovo').innerHTML = `Nasumicno izabrano slovo je:  <strong class='badge badge-primary text-wrap' ><h5>${radnomSlovo}</h5></strong>`;
 
-//     })
-//     .catch(function (error) {
-//         console.log("Error getting documents: ", error);
-//     });
+    formStartGame.addEventListener('submit', e => {
+        e.preventDefault();
+        let formatiranUserInput = [];
+        let pocetnaSlova = [];
+        let abeceda = ['A', 'B', 'C', 'Č', 'Ć', 'D', 'Dž', 'Đ', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Lj', 'N', 'M', 'Nj', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+        let randomSlovo = abeceda[Math.floor(Math.random() * abeceda.length)];
+
+        // Formatted input from user - no whitespacing, every first letter to upper case
+        formInputs.forEach(input => {
+            formatiranUserInput.push(
+                input.value.replace(/\s+/g, '').charAt(0).toUpperCase() + input.value.replace(/\s+/g, '').toLowerCase().slice(1)
+            );
+        });
+
+        // Every first letter sliced and added to separate array
+        formatiranUserInput.forEach(input => {
+            //console.log(input.slice(0, 2))
+            if (input.slice(0, 2) == 'Nj' || input.slice(0, 2) == 'Lj' || input.slice(0, 2) == 'Dž') {
+                pocetnaSlova.push(input.slice(0, 2));
+            } else {
+                pocetnaSlova.push(input.charAt(0));
+            }
+
+        });
+
+
+        // User compare logic
+        pojmovi.where('pocetnoSlovo', '==', 'A').get()
+            .then(snapshot => {
+                let poklopoljeniPojmoviUsera = [];
+
+                snapshot.docs.forEach(doc => {
+                    poklopoljeniPojmoviUsera.push(doc.data().pojam);
+                })
+                //console.log(poklopoljeniPojmoviUsera.filter(x => formatiranUserInput.includes(x)))
+            });
+
+
+        pojmovi.where('pocetnoSlovo', '==', 'A').get()
+            .then(snapshot => {
+                let CPUDrzava;
+                let pojmoviArray = [];
+                snapshot.docs.forEach(doc => {
+                    if (doc.data().kategorija == 'Država') {
+                        let chance = Math.round(Math.random() * 99) + 1;
+                        pojmoviArray.push(doc.data().pojam);
+                        
+                        if (chance <= 80) {
+                            //console.log(typeof (doc.data()))
+                            CPUDrzava = pojmoviArray[Math.floor(Math.random() * pojmoviArray.length)];
+                        } else {
+                            CPUDrzava = 'Nemam pojam!';
+                        }
+                        //console.log(CPUDrzava);
+                    }
+                });
+                console.log(CPUDrzava);
+            });
+
+        //console.log(pocetnaSlova);
+        //console.log(formatiranUserInput);
+        //console.log(poklopoljeniPojmoviUsera);
+    })
+
+}
