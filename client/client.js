4730d4e5ecedee3ff0bcbba27e28b1cb637af41d
Filip Pesic <filip.pesic.dev@gmail.com>
2020-06-05 15:05:39 +0200

    alpha test initial commit

--- client/client.js
+++ client/client.js
@@ -1,11 +1,18 @@
+let formStartGame = document.querySelector('#formStartGame');
+let formInputs = document.querySelectorAll('#formStartGame input');
+let alphabet = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B',
+    'C', 'Č', 'Ć', 'D', 'Dž', 'Đ', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Lj', 'N', 'M', 'Nj', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+let termsCollection = db.collection('pojmovi');
+
 const writeEvent = (text) => {
     // ul elemet
     const parent = document.querySelector('#events');
-    
+
     // li element
     const el = document.createElement('li');
     el.innerHTML = text;
-    
+
     parent.appendChild(el);
 };
 
@@ -19,23 +26,75 @@ const onFormSubmitted = e => {
     socket.emit('message', text);
 };
 
-const addButtonListeners = () => {
-    // console.log('ovde smo burke');
-    ['rock', 'paper', 'scissors'].forEach((id) => {
-        const button = document.getElementById(id);
-        
-        button.addEventListener('click', () => {
-            socket.emit('turn', id);
-        })
-    });
-};
 
-writeEvent('Welcome to RPS');
+formStartGame.addEventListener('submit', e => {
+    e.preventDefault();
+    let formattedUserInput = [];
+
+    formInputs.forEach(input => {
+        formattedUserInput.push(input.value.replace(/\s+/g, '').charAt(0).toUpperCase() + input.value.replace(/\s+/g, '').toLowerCase().slice(1));
+    });
+
+    formattedUserInput.splice(-1, 1);
+    console.log(formattedUserInput, 'formattedUserInput');
+
+    let checkedUserInput = [];
+
+    let Drzava = '';
+    let Reka = '';
+    let Planina = '';
+    let Grad = '';
+    let Biljka = '';
+    let Zivotinja = '';
+    let Predmet = '';
+
+    termsCollection.where('pocetnoSlovo', '==', 'A')
+        .get()
+        .then(snapshot => {
+            let DBTerms = [];
+            snapshot.docs.forEach(doc => {
+                console.log('unutar fora');
+                DBTerms.push(doc.data().pojam);
+                //console.log(DBTerms, 'DBTerms');
+                if (doc.data().kategorija === 'Država' && DBTerms.includes(formattedUserInput[0])) { // bad logic
+                    Drzava = formattedUserInput[0];
+                    console.log(Drzava, 'DRZAVA U IFU');
+                }
+                if (doc.data().kategorija === 'Reka' && DBTerms.includes(formattedUserInput[1])) {
+                    Reka = formattedUserInput[1];
+                }
+                if (doc.data().kategorija === 'Planina' && DBTerms.includes(formattedUserInput[2])) {
+                    Planina = formattedUserInput[2];
+                }
+                if (doc.data().kategorija === 'Grad' && DBTerms.includes(formattedUserInput[3])) {
+                    Grad = formattedUserInput[3];
+                }
+                if (doc.data().kategorija === 'Biljka' && DBTerms.includes(formattedUserInput[4])) {
+                    Biljka = formattedUserInput[4];
+                }
+                if (doc.data().kategorija === 'Životinja' && DBTerms.includes(formattedUserInput[5])) {
+                    Zivotinja = formattedUserInput[5];
+                }
+                if (doc.data().kategorija === 'Predmet' && DBTerms.includes(formattedUserInput[6])) {
+                    Predmet = formattedUserInput[6];
+                }
+            });
+            console.log(checkedUserInput, 'checkedUserInput');
+            checkedUserInput.push(Drzava, Reka, Planina, Grad, Biljka, Zivotinja, Predmet);
+            return checkedUserInput;
+        })
+        .then( data => {
+            socket.emit('turn', data)
+            console.log(data, 'data');
+        }
+        ); // then
+    //console.log(DBTerms, 'DBTerms');
+});
+
+writeEvent('Welcome to ZGeo');
 
 const socket = io();
 socket.on('message', writeEvent);
 
 document.querySelector('#chat-form')
-.addEventListener('submit', onFormSubmitted);
-
-addButtonListeners();
+    .addEventListener('submit', onFormSubmitted);
--- server/rps-game.js
+++ server/rps-game.js
@@ -2,14 +2,17 @@ class Rpsgame {
     constructor(p1, p2) {
         this._players = [p1, p2];
         this._turns = [null, null];
+        this._points = [0, 0];
 
-        this._sendToPlayers('RPS starts..');
+
+        this._sendToPlayers('Zanimljiva geografija pocinje..');
 
         this._players.forEach((player, idx) => {
             player.on('turn', turn => {
-              this._onTurn(idx, turn);
+                //console.log(turn, 'turn  server');
+                this._onTurn(idx, turn);
             });
-          });
+        });
     }
 
     _sendToPlayer(playerIndex, msg) {
@@ -23,40 +26,62 @@ class Rpsgame {
     }
 
     _onTurn(playerIndex, turn) {
-        this._turns[playerIndex] = turn;
-        this._sendToPlayer(playerIndex, `You selected ${turn}`);
+        this._turns[playerIndex] = turn; // adds a checkedUserInput from the client side
+        this._sendToPlayer(playerIndex, `Uneli ste vasa polja`);
 
-        this._checkGameOver();
+        if((this._turns[0] && this._turns[1]) != null) {
+            this._getGameResult();
+            this._checkGameOver();
+        }
     }
 
     _checkGameOver() {
-        const turns = this._turns;
-
-        if(turns[0] && turns[1]) {
-            this._sendToPlayers('Game Over: ' + turns.join(' : '));
+        // const turns = this._turns;
+        console.log('check game over');
+            this._sendToPlayers('Game Over: ', this._points.join(' <---> '));
             this._getGameResult();
-            this._turns = [null, null];
-            this._sendToPlayers('<--------------------Next Round------------------->');
-        }
+            //this._turns = [null, null];
+            this._sendToPlayers('<---------------------Next Round--------------------->');
     }
 
     _getGameResult() {
-        const p0 = this._decodeTurn(this._turns[0]);
-        const p1 = this._decodeTurn(this._turns[1]);
+        console.log((this._turns[0] && this._turns[1]) !== (null || undefined));
+        // if((this._turns[0] && this._turns[1]) != null) {
+            // kad jedan submit onda se odmah proveri, a ne kad oba submit
 
-        const distance = (p1 - p0 + 3) % 3;
+            for (let i = 0; i < this._turns.length; i++) {
+                for (let j = 1; j <= this._turns[i].length; j++) {
+                    if (this._turns[i][i] === this._turns[j][i]) {
+                        this._points[0] += 5;
+                        this._points[1] += 5;
+                    } else if (this._turns[i][i] !== this._turns[j][i]) {
+                        this._points[0] += 10;
+                        this._points[1] += 10;
 
-        switch (distance) {
-            case 0:
-                this._sendToPlayers('Draw!');
-                break;
-            case 1:
-                this._sendWinMessage(this._players[0], this._players[1]);
-                break;
-            case 2:
-                this._sendWinMessage(this._players[1], this._players[0]);
-                break;
-        }
+                    } else if (this._turns[i][i] !== '' && this._turns[j][i] === '') {
+                        this._points[0] += 15;
+                    } else if (this._turns[i][i] === '' && this._turns[j][i] !== '') {
+                        this._points[1] += 15;
+                    }
+                }
+            }
+        // }
+        // const p0 = this._decodeTurn(this._turns[0]);
+        // const p1 = this._decodeTurn(this._turns[1]);
+
+        // const distance = (p1 - p0 + 3) % 3;
+
+        // switch (distance) {
+        //     case 0:
+        //         this._sendToPlayers('Draw!');
+        //         break;
+        //     case 1:
+        //         this._sendWinMessage(this._players[0], this._players[1]);
+        //         break;
+        //     case 2:
+        //         this._sendWinMessage(this._players[1], this._players[0]);
+        //         break;
+        // }
     }
 
     _sendWinMessage(winner, loser) {
@@ -64,18 +89,18 @@ class Rpsgame {
         loser.emit('message', 'You lost!');
     }
 
-    _decodeTurn(turn) {
-        switch (turn) {
-            case 'rock':
-                return 0;
-            case 'scissors':
-                return 1;
-            case 'paper':
-                return 2;
-            default:
-                throw new Error(`Could not decode turn ${turn}`);
-        }
-    }
+    // _decodeTurn(turn) {
+    //     switch (turn) {
+    //         case 'rock':
+    //             return 0;
+    //         case 'scissors':
+    //             return 1;
+    //         case 'paper':
+    //             return 2;
+    //         default:
+    //             throw new Error(`Could not decode turn ${turn}`);
+    //     }
+    // }
 }
 
 module.exports = Rpsgame;
